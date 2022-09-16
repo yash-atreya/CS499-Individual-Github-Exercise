ee23cff5424fa4edb0bcbf9c69ddb943eb62dee1
Yash Atreya <yash@Yashs-MacBook-Air.local>
2022-09-16 11:43:48 -0700

    Hello World

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ array.sort((a, b) => a - b) // Sorting in ascending order
 // array.sort((a, b) => b - a) // For sorting in descending order 
 
 console.log('Sorted Array: ', array);
+
+// Hello World!!
