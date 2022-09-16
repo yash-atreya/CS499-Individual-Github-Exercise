af42e3579f0c04a74b4ae7a11c625854c0a3eace
Yash Atreya <yash@Yashs-MacBook-Air.local>
2022-09-16 10:57:19 -0700

    Populated array and added sorting functionality using array.sort()

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 //
 
-const array = []; // Declared array
+const array = [1, 30, 4, 21, 100000]; // Declared array
 
-console.log('Array: ', array); // Print Array
+console.log('Unsorted Array: ', array); // Print Array
+
+array.sort((a, b) => a - b) // Sorting in ascending order
+// array.sort((a, b) => b - a) // For sorting in descending order 
+
+console.log('Sorted Array: ', array);
